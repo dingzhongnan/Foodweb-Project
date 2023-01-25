d05b510f0fff7bac9be2549c0e15b2ba75273d43
DingZhongNan <dingzhongnancn@gmail.com>
2023-01-25 18:28:01 +0800

    20230125

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
-const toggleBtn = document.querySelector('.navbar_toggle_Btn');
-const menu = document.querySelector('.navbar_menu');
-const register = document.querySelector('.navbar_register');
+// const toggleBtn = document.querySelector('.navbar_toggle_Btn');
+// const menu = document.querySelector('.navbar_menu');
+// const register = document.querySelector('.navbar_register');
 
-toggleBtn.addEventListener('click', () => {
-  menu.classList.toggle('active');
-  register.classList.toggle('active');
-});
+// toggleBtn.addEventListener('click', () => {
+//   menu.classList.toggle('active');
+//   register.classList.toggle('active');
+// });
+
+const hamburger = document.querySelector(".hamburger");
+const navbar_menu = document.querySelector(".navbar_menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active")
+  navbar_menu.classList.toggle("active")
+})
